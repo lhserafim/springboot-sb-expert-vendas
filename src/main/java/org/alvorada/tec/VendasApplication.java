1f94529c24141bdea2e4f6f4d8739d9429b93b8e
Luiz Henrique Sanches Serafim <a29061@somoseducacao.com.br>
2021-04-08 09:05:28 -0300

    Fazendo um hello world, com controller

--- src/main/java/org/alvorada/tec/VendasApplication.java
+++ src/main/java/org/alvorada/tec/VendasApplication.java
@@ -2,9 +2,17 @@ package org.alvorada.tec;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class VendasApplication {
+
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "Hello World - Spring Boot";
+    }
     //Eu digitei psvm e o intelliJ transformou em public static....
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
