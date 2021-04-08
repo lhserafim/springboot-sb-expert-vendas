9d9c4e951b9f7783ff6f638b5c6b7c6d76e93bad
Luiz Henrique Sanches Serafim <a29061@somoseducacao.com.br>
2021-04-08 08:32:27 -0300

    Configuração básica para iniciar um projeto em SB

---
+++ src/main/java/org/alvorada/tec/VendasApplication.java
@@ -0,0 +1,12 @@
+package org.alvorada.tec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendasApplication {
+    //Eu digitei psvm e o intelliJ transformou em public static....
+    public static void main(String[] args) {
+        SpringApplication.run(VendasApplication.class, args);
+    }
+}
